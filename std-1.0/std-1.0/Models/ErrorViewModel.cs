b81e03c52c00d924607e2589333fcbbc285e460d
Diego <diego.casa@tecsup.edu.pe>
2017-11-14 09:48:31 -0500

    Vistas

---
+++ std-1.0/std-1.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace std_1._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
